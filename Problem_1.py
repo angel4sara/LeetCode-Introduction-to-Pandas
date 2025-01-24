3a310c37858fd380586d2d8ef162ee9326a42b67
angel4sara <158163802+angel4sara@users.noreply.github.com>
2025-01-24 12:24:39 +0000

    Create Problem_1.py

---
+++ Problem_1.py
@@ -0,0 +1,9 @@
+-- # Problem Name: Create a DataFrame from List
+-- ## LeetCode Link: https://leetcode.com/problems/create-a-dataframe-from-list/description/?envType=study-plan-v2&envId=introduction-to-pandas&lang=pythondata
+-- 
+----
+import pandas as pd
+
+def createDataframe(student_data: List[List[int]]) -> pd.DataFrame:
+    return pd.DataFrame(student_data, columns = ['student_id', 'age'])
+    
