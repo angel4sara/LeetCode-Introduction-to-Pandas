beb14c357ab16e183bebe61a8c9e7635877a2718
angel4sara <158163802+angel4sara@users.noreply.github.com>
2025-01-26 21:38:09 +0000

    Create Problem_6.py

---
+++ Problem_6.py
@@ -0,0 +1,8 @@
+-- # Problem Name: Drop Duplicate Rows
+-- ## LeetCode Link: https://leetcode.com/problems/drop-duplicate-rows/description/?envType=study-plan-v2&envId=introduction-to-pandas&lang=pythondata
+----
+import pandas as pd
+
+def dropDuplicateEmails(customers: pd.DataFrame) -> pd.DataFrame:
+    customers = customers.drop_duplicates(subset ='email', keep='first')
+    return customers
