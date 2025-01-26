c155020204ef8606d61d999590ec916e119c99cd
angel4sara <158163802+angel4sara@users.noreply.github.com>
2025-01-26 21:36:59 +0000

    Create Problem_5.py

---
+++ Problem_5.py
@@ -0,0 +1,8 @@
+-- # Problem Name: Create a New Column
+-- ## LeetCode Link: https://leetcode.com/problems/create-a-new-column/description/?envType=study-plan-v2&envId=introduction-to-pandas&lang=pythondata
+----
+import pandas as pd
+
+def createBonusColumn(employees: pd.DataFrame) -> pd.DataFrame:
+    employees['bonus'] = employees['salary'] * 2
+    return employees
