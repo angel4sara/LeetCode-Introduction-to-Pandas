53ac85db246aa1be7e1d68dd30641d130474c98b
angel4sara <158163802+angel4sara@users.noreply.github.com>
2025-01-27 09:22:38 +0000

    Create Problem_12.py

---
+++ Problem_12.py
@@ -0,0 +1,8 @@
+-- # Problem Name: Reshape Data: Concatenate
+-- ## LeetCode Link: https://leetcode.com/problems/reshape-data-concatenate/description/?envType=study-plan-v2&envId=introduction-to-pandas&lang=pythondata
+----
+import pandas as pd
+
+def concatenateTables(df1: pd.DataFrame, df2: pd.DataFrame) -> pd.DataFrame:
+    df3 = pd.concat([df1, df2])
+    return df3
