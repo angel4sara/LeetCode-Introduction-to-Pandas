5808febcc2ae56c4bf30b70c2e7c767f7e097179
angel4sara <158163802+angel4sara@users.noreply.github.com>
2025-01-27 09:23:40 +0000

    Create Problem_13.py

---
+++ Problem_13.py
@@ -0,0 +1,8 @@
+-- # Problem Name: Reshape Data: Pivot
+-- ## LeetCode Link: https://leetcode.com/problems/reshape-data-pivot/description/?envType=study-plan-v2&envId=introduction-to-pandas&lang=pythondata
+----
+import pandas as pd
+
+def pivotTable(weather: pd.DataFrame) -> pd.DataFrame:
+    df_pivot1 = weather.pivot(index="month", columns="city", values="temperature")
+    return df_pivot1
