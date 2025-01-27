3ccb3c4c7882a162254ae7e57f4b47991a4eb807
angel4sara <158163802+angel4sara@users.noreply.github.com>
2025-01-27 09:15:10 +0000

    Create Problem_7.py

---
+++ Problem_7.py
@@ -0,0 +1,8 @@
+-- # Problem Name: Drop Missing Data
+-- ## LeetCode Link: https://leetcode.com/problems/drop-missing-data/description/?envType=study-plan-v2&envId=introduction-to-pandas&lang=pythondata
+----
+import pandas as pd
+
+def dropMissingData(students: pd.DataFrame) -> pd.DataFrame:
+    students = students.dropna(subset =['name'])
+    return students
