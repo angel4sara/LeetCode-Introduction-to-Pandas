1749a1cd6c84e74eaba2137d560c1aaeea8197ae
angel4sara <158163802+angel4sara@users.noreply.github.com>
2025-02-01 14:34:56 +0000

    Update Problem_6.py

--- Problem_6.py
+++ Problem_6.py
@@ -1,5 +1,6 @@
 -- # Problem Name: Drop Duplicate Rows
 -- ## LeetCode Link: https://leetcode.com/problems/drop-duplicate-rows/description/?envType=study-plan-v2&envId=introduction-to-pandas&lang=pythondata
+--
 ----
 import pandas as pd
 
