c0307820ce4eb3b197cd14f253ed9ba6ae6cf74b
angel4sara <158163802+angel4sara@users.noreply.github.com>
2025-02-02 22:07:52 +0000

    Update Problem_13.py

--- Problem_13.py
+++ Problem_13.py
@@ -1,6 +1,5 @@
 -- # Problem Name: Reshape Data: Pivot
 -- ## LeetCode Link: https://leetcode.com/problems/reshape-data-pivot/description/?envType=study-plan-v2&envId=introduction-to-pandas&lang=pythondata
---
 ----
 import pandas as pd
 
