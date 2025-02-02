5b38cc8da14adaeadc343dc0c2ad8f34c56dc8d8
angel4sara <158163802+angel4sara@users.noreply.github.com>
2025-02-02 22:07:12 +0000

    Update Problem_1.py

--- Problem_1.py
+++ Problem_1.py
@@ -1,6 +1,5 @@
 -- # Problem Name: Create a DataFrame from List
 -- ## LeetCode Link: https://leetcode.com/problems/create-a-dataframe-from-list/description/?envType=study-plan-v2&envId=introduction-to-pandas&lang=pythondata
---
 ----
 import pandas as pd
 
