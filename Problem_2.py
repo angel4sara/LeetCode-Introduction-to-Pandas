854baf72f4f829c81519fb11d6068e92ea284084
angel4sara <158163802+angel4sara@users.noreply.github.com>
2025-02-04 21:21:35 +0000

    Update Problem_2.py

--- Problem_2.py
+++ Problem_2.py
@@ -1,6 +1,5 @@
 -- # Problem Name: Get the Size of a DataFrame
 -- ## LeetCode Link: https://leetcode.com/problems/get-the-size-of-a-dataframe/description/?envType=study-plan-v2&envId=introduction-to-pandas&lang=pythondata
---
 ----
 import pandas as pd
 
