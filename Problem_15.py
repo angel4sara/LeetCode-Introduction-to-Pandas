ae43328bfdbe23a664edb65db24133e7620550f7
angel4sara <158163802+angel4sara@users.noreply.github.com>
2025-02-04 21:21:22 +0000

    Update Problem_15.py

--- Problem_15.py
+++ Problem_15.py
@@ -1,6 +1,5 @@
 -- # Problem Name: Method Chaining
 -- ## LeetCode Link: https://leetcode.com/problems/method-chaining/description/?envType=study-plan-v2&envId=introduction-to-pandas&lang=pythondata
---
 ----
 import pandas as pd
 
