a8785676d7308f93ab2c9f734abb6d0bfa244284
angel4sara <158163802+angel4sara@users.noreply.github.com>
2025-02-08 19:53:28 +0000

    Update Problem_9.py

--- Problem_9.py
+++ Problem_9.py
@@ -1,6 +1,5 @@
 -- # Problem Name: Rename Columns
 -- ## LeetCode Link:https://leetcode.com/problems/rename-columns/description/?envType=study-plan-v2&envId=introduction-to-pandas&lang=pythondata
---
 ----
 import pandas as pd
 
